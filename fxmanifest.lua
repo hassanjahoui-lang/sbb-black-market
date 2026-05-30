833d50c5305abb21c4720b3c15dbfa4fab872b82
hassanjahoui-lang <hassan.jahoui@persolinostrocchi.istruzioneer.it>
2026-05-30 09:13:56 +0200

    Create  fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Il tuo nome'
+description 'Script Mercato Nero per QBCore'
+version '1.0.0'
+
+shared_script 'config.lua'
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
